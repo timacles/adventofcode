72042362c82a5e76b16fb456f2fe37abe6aa62b8
tim a <timfosho@gmail.com>
2023-12-22 23:03:42 -0500

    file iter

--- day1/main.rs
+++ day1/main.rs
@@ -1,9 +1,10 @@
-//use std::env;
-use std::fs;
+use std::fs::File;
 use regex::Regex;
+use std::io::{self, BufRead};
 
 fn match_digit(pattern: Regex, in_line: String) -> String {
-    for int in pattern.find_iter(&in_line) {
+    let mut matched_integers = Vec::new();
+    for int in pattern.cap(&in_line) {
         println!("Found integer: {}", int.as_str());
         return int.as_str().to_string();
     }
@@ -11,18 +12,30 @@ fn match_digit(pattern: Regex, in_line: String) -> String {
 }
 
 
-fn main() {
+fn main() -> io::Result<()> {
     let file_path = "example1.txt";
     let pattern = Regex::new(r"[0-9]").unwrap();
 
-    // --snip--
     println!("In file {}", file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
 
-    println!("\n===\n\n{contents}");
+    let mut count = 0;
 
+    for line in reader.lines() {
+        match line {
+            Ok(line_content) => {
+                count += 1;
+                let out = match_digit(pattern, line_content);
+                println!("{}: {}", count, out);
+            }
+            Err(err) => {
+                eprintln!("Error reading line: {}", err);
+            }
+        }
+    }
 
-    match_digit(pattern, contents);
+    
+    Ok(())
 }
