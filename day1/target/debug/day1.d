b499d5b546f84f7b2e5d995cea506e226c7326a3
tim a <timfosho@gmail.com>
2023-12-22 23:04:37 -0500

    file iter

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day1\target\debug\day1.exe: C:\Users\Tim\Desktop\git\aoc\day1\main.rs
--- day1/target/debug/deps/day1-ca1b4f78e43829b4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day1\target\debug\deps\libday1-ca1b4f78e43829b4.rmeta: main.rs
-
-C:\Users\Tim\Desktop\git\aoc\day1\target\debug\deps\day1-ca1b4f78e43829b4.d: main.rs
-
-main.rs:
