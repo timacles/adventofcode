c26fa61e10d9cb5e60617a88cd8b85d0ffebf45f
tim a <timfosho@gmail.com>
2024-01-02 22:35:27 -0500

    solved day 2

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day2\target\debug\day2.exe: C:\Users\Tim\Desktop\git\aoc\day2\src\main.rs
--- day2/target/debug/deps/day2-96e23dc32bb311e5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day2\target\debug\deps\day2-96e23dc32bb311e5.exe: src\main.rs
-
-C:\Users\Tim\Desktop\git\aoc\day2\target\debug\deps\day2-96e23dc32bb311e5.d: src\main.rs
-
-src\main.rs:
