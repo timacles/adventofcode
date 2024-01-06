433e8ee1b09c33c13ad4753be52911411c9e05de
tim a <timfosho@gmail.com>
2024-01-05 22:21:45 -0500

    add boundaries

--- day3/target/debug/day3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day3\target\debug\day3.exe: C:\Users\Tim\Desktop\git\aoc\day3\src\main.rs
--- day3/target/debug/deps/day3-9e7760eac41723d5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tim\Desktop\git\aoc\day3\target\debug\deps\day3-9e7760eac41723d5.exe: src\main.rs
-
-C:\Users\Tim\Desktop\git\aoc\day3\target\debug\deps\day3-9e7760eac41723d5.d: src\main.rs
-
-src\main.rs:
